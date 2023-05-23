89b2ade52164b19f40dc078f07fcd29875a272e7
murim77 <murim77@naver.com>
2023-05-23 15:26:25 +0900

    coding

---
+++ candidates/candidates.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"os"
+)
+
+func main() {
+	if len(os.Args) < 3 {
+		fmt.Fprintln(os.Stderr, "Invalid arguments!\nlottery filename count")
+		return
+	}
+
+	filename := os.Args[1]
+	count, err := strnconv.Atoi(os.Args[2])
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "cannot convert count to integer! count:", count)
+		return
+	}
+
+	candidates, err := readCandidates(filename)
+	winners := make([]string, count)
+	for i := 0; i < count; i++ {
+		n := rand.Intn(len(candidates))
+		winners[i] = candidates[n]
+		candidates = append(candidates[:n], candidates[n+1:]...)
+	}
+
+	fmt.Println("Winners are !!!")
+	for _, winner := range winners {
+		fmt.Println(winner)
+	}
+}
